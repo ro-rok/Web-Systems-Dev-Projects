2328139d2627b46ab15be6c5c0e3e30dcb6bf9f3
Rohan Khanna <rk868@njit.edu>
2023-10-16 19:26:08 -0400

    Added 2 extra tests for zero divison error

--- M4/test_MyCalc.py
+++ M4/test_MyCalc.py
@@ -60,19 +60,27 @@ def test_ans_mult(my_calc_instance, ans, b, expected):
 # rk868 16/10/2023: Test cases for number-divide-number operation using parametrize decorator to run the same test case with different inputs
 #  It verifies that the calc function correctly calculates the quotient and compares it to the expected result.
 #  The test case will pass if the actual output is equal to the expected output and fail otherwise.
-# It also tests that the calc function raises a ZeroDivisionError exception when the second argument is zero.
-@pytest.mark.parametrize("a, b, expected", [(64, 4, 16), (0, 1, 0), (-100, 100, -1), (-1.1, 1.1, -1.0)])
+@pytest.mark.parametrize("a, b, expected", [(64, 4, 16), (-100, 100, -1), (-1.1, 1.1, -1.0)])
 def test_div(my_calc_instance, a, b, expected):
     result = my_calc_instance.calc(a, b, "/")
     assert result == expected
 
+# rk868 16/10/2023: Confirm that the calc function raises a ZeroDivisionError exception when the divisor is zero.
+def test_div_zero(my_calc_instance):
+    with pytest.raises(ZeroDivisionError):
+        my_calc_instance.calc(1, 0, "/")
+
 # rk868 16/10/2023: Test cases for ans-divide-number operation using parametrize decorator to run the same test case with different inputs
 #  It sets the value of the "ans" attribute of the MyCalc instance and ensures that the calc function correctly uses it to calculate the quotient.
 #  The test case will pass if the actual output is equal to the expected output and fail otherwise.
-# It also tests that the calc function raises a ZeroDivisionError exception when the second argument is zero.
-@pytest.mark.parametrize("ans, b, expected", [(32, 2, 16), (0, 1, 0), (-100, 100, -1), (-2.5, 2.5, -1.0)])
+@pytest.mark.parametrize("ans, b, expected", [(32, 2, 16), (-100, 100, -1), (-2.5, 2.5, -1.0)])
 def test_ans_div(my_calc_instance, ans, b, expected):
     my_calc_instance.ans = ans
     result = my_calc_instance.calc("ans", b, "/")
     assert result == expected
 
+# rk868 16/10/2023: Confirm that the calc function raises a ZeroDivisionError exception when the denominator is zero.
+def test_ans_div_zero(my_calc_instance):
+    my_calc_instance.ans = 1
+    with pytest.raises(ZeroDivisionError):
+        my_calc_instance.calc("ans", 0, "/")
