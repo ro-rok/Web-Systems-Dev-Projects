8bbed857a2994f8ce9b0c0e80a76ae756a43b1e0
Rohan Khanna <rk868@njit.edu>
2023-12-06 22:22:32 -0500

    SQL Queries for creating Tables

---
+++ Spotify_Project/sql/008_create_table_artists.sql
@@ -0,0 +1,14 @@
+CREATE TABLE
+    IS601_Artists (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        artist_id VARCHAR(50) UNIQUE NOT NULL,
+        artist_name VARCHAR(255) NOT NULL CHECK (artist_name <> ''),
+        artist_popularity INT CHECK (artist_popularity >= 0 AND artist_popularity <= 100) DEFAULT 0,
+        preview_url VARCHAR(255) CHECK (preview_url <> ''),
+        followers_total INT CHECK (followers_total >= 0),
+        artist_uri VARCHAR(100) CHECK (artist_uri <> ''),
+        artist_img VARCHAR(255) CHECK (artist_img <> ''),
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        UNIQUE KEY (artist_id)
+    );
