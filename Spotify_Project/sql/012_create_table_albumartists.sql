703d1e9e8ef6118693fd803fbd90902f4db72acf
Rohan Khanna <rk868@njit.edu>
2023-12-10 15:43:35 -0500

    SQL Tables edited

--- Spotify_Project/sql/012_create_table_albumartists.sql
+++ Spotify_Project/sql/012_create_table_albumartists.sql
@@ -1,11 +1,11 @@
 CREATE TABLE
     IS601_ArtistAlbums(
         id int auto_increment PRIMARY KEY,
-        artist_id int not null,
-        album_id int not null,
+        artist_id VARCHAR(60) not null,
+        album_id VARCHAR(60) not null,
         created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         UNIQUE KEY (artist_id, album_id),
-        FOREIGN KEY (artist_id) REFERENCES IS601_Artists(id),
-        FOREIGN KEY (album_id) REFERENCES IS601_Albums(id)
+        FOREIGN KEY (artist_id) REFERENCES IS601_Artists(artist_id),
+        FOREIGN KEY (album_id) REFERENCES IS601_Albums(album_id)
     );
--- Spotify_Project/sql/013_create_table_artisttracks.sql
+++ Spotify_Project/sql/013_create_table_artisttracks.sql
@@ -1,11 +1,11 @@
 CREATE TABLE
-    IS601_ArtistGenres(
+    IS601_ArtistTracks(
         id int auto_increment PRIMARY KEY,
         artist_id VARCHAR(60) not null,
-        genre_name VARCHAR(100) not null,
+        track_id VARCHAR(60) not null,
         created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        UNIQUE KEY (artist_id, genre_id),
+        UNIQUE KEY (artist_id, track_id),
         FOREIGN KEY (artist_id) REFERENCES IS601_Artists(artist_id),
-        FOREIGN KEY (genre_name) REFERENCES IS601_Genres(genre_name) 
+        FOREIGN KEY (track_id) REFERENCES IS601_Tracks(track_id)
     );
